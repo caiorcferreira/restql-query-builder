f2d6ebe2dc743abcf678ba9b7d1d4d67600ab583
Caio Ferreira <caio.cavalcante@b2wdigital.com>
2018-05-26 23:50:11 -0300

    [add] implementa helper para transformar um objeto em um function builder

--- src/builder.js
+++ src/builder.js
@@ -1,6 +1,6 @@
 import { curry, map, apply, cond, is, identity as I, always as K, call as S } from 'ramda';
 
-const toArray = cond([[is(Array), I], [K(true), Array.of]]);
+export const toBuilder = cond([[is(Function), I], [K(true), K]]);
 
 export const run = curry((reducer, builder, accumulatedInput) => {
   return reducer(accumulatedInput, builder());
